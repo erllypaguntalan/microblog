f82f4323ddcc3de7896f3433f14775a325ce5df0
erllypaguntalan <e.paguntalan@klab.com>
2016-01-25 16:08:30 +0800

    Lesson13 - Dates and Times

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ OPENID_PROVIDERS = [
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
-WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 
 # mail server settings
 MAIL_SERVER = 'smtp.gmail.com'
